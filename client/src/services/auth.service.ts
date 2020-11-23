4c3e0005ed1a1f07862318e98152e1dad45ad17c
Roberto Rossini <robertorossini96@gmail.com>
2020-11-23 11:26:06 +0100

    Test lint hook

--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = "http://localhost:8080/api/auth/";
-
+ 
 class AuthService {
   login = async (username, password) => {
     const response = await axios
